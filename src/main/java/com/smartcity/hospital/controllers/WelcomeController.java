ac0afa973b29915c5c0bc5a14af59fdb22a4d3b7
ShivamVerma380 <shivam.r.verma@seagate.com>
2024-01-13 14:00:40 +0530

    Added default api

---
+++ src/main/java/com/smartcity/hospital/controllers/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.smartcity.hospital.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartcity.hospital.helper.ResponseMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+    
+    @Autowired
+    private ResponseMessage responseMessage;
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseMessage> getWelcomeMessage() {
+        try {
+            responseMessage.setMessage("Welcome");
+            return ResponseEntity.status(HttpStatus.OK).body(responseMessage);        
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseMessage.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+    }
+}

65b110f7addad667d41221a499d93b0ba149d15e
ShivamVerma380 <shivam.r.verma@seagate.com>
2024-01-14 15:03:19 +0530

    Added hospital

---
+++ src/main/java/com/smartcity/hospital/controller/hospital/HospitalController.java
@@ -0,0 +1,53 @@
+package com.smartcity.hospital.controller.hospital;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.smartcity.hospital.helper.ResponseMessage;
+import com.smartcity.hospital.model.Hospital;
+import com.smartcity.hospital.services.hospital.HospitalService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HospitalController {
+    
+
+    @Autowired
+    private ResponseMessage responseMessage;
+
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @PostMapping("/hospital")
+    public ResponseEntity<?> addHospital(@RequestHeader("Authorization") String authorization, @RequestBody Hospital hospital) {
+        try {
+            return hospitalService.addHospital(authorization, hospital);
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseMessage.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+    }
+
+    @PostMapping("/hospital/image")
+    public ResponseEntity<?> addHospital(@RequestHeader("Authorization") String authorization,@RequestParam("id") int id, @RequestParam("image") MultipartFile image) {
+        try {
+            return hospitalService.addHospitalImage(authorization, id, image);
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseMessage.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+    }
+    
+
+}
---
+++ src/main/java/com/smartcity/hospital/services/hospital/HospitalService.java
@@ -0,0 +1,93 @@
+package com.smartcity.hospital.services.hospital;
+
+import java.sql.Blob;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.smartcity.hospital.dao.CitizenDao;
+import com.smartcity.hospital.dao.HospitalDao;
+import com.smartcity.hospital.helper.JwtUtil;
+import com.smartcity.hospital.helper.ResponseMessage;
+import com.smartcity.hospital.model.Hospital;
+
+@Component
+public class HospitalService {
+    
+    @Autowired
+    private ResponseMessage responseMessage;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CitizenDao citizenDao;
+
+    @Autowired
+    private HospitalDao hospitalDao;
+
+    @PersistenceContext
+    public EntityManager entityManager;
+
+
+    public ResponseEntity<?> addHospital(String authorization, Hospital hospital) {
+        try {
+            String token = authorization.substring(7);
+
+            String email = jwtUtil.extractUsername(token); 
+            
+            if(citizenDao.getCitizenByemail(email)!=null) {
+                responseMessage.setMessage("You aren't authorised to add hospitals......");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseMessage);
+            }
+
+            hospitalDao.save(hospital);
+
+            responseMessage.setMessage("Hospital added successfully......");
+            
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            responseMessage.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+        
+    }
+
+    public ResponseEntity<?> addHospitalImage(String authorization, int id, MultipartFile image) {
+        try {
+            String token = authorization.substring(7);
+
+            String email = jwtUtil.extractUsername(token); 
+            
+            if(citizenDao.getCitizenByemail(email)!=null) {
+                responseMessage.setMessage("You aren't authorised to add hospitals......");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseMessage);
+            }
+            Hospital hospital = hospitalDao.getHospitalById(id);
+            if (hospital==null) {
+                responseMessage.setMessage("Hospital does not exist........");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMessage);
+            }
+            Session session = entityManager.unwrap(Session.class);
+            Blob hospitalImage = session.getLobHelper().createBlob(image.getInputStream(),image.getSize());
+            hospital.setImg(hospitalImage);
+            hospitalDao.save(hospital);
+            responseMessage.setMessage("Image added successfully....");
+            return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            responseMessage.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+    }
+}
